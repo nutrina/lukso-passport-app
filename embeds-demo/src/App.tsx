21cd4910b8e738d677ab0b4189cd35d5ed89f32f
Gerald Iakobinyi-Pich <nutrina9@gmail.com>
2025-06-16 22:29:33 +0300

    fix: build errors

--- embeds-demo/src/App.tsx
+++ embeds-demo/src/App.tsx
@@ -1,17 +1,11 @@
 import { useState } from "react";
-import {
-  LightTheme,
-  PassportScoreWidget,
-  usePassportScore,
-} from "@passportxyz/passport-embed";
+import { LightTheme, PassportScoreWidget } from "@passportxyz/passport-embed";
 import { Buffer } from "buffer";
 import "./App.css";
-import { ethers } from 'ethers';
+import { ethers } from "ethers";
 // const provider = new ethers.BrowserProvider(window.lukso);
 
 // const accounts = await provider.send('eth_requestAccounts', []);
-
-
 
 const PASSPORT_API_KEY = import.meta.env.VITE_SCORER_ID || "DEMO.API_KEY";
 const PASSPORT_SCORER_ID = import.meta.env.VITE_API_KEY || "335"; // TODO: use DEMO after fix
@@ -29,14 +23,13 @@ const connectWallet = async () => {
     return;
   }
   const provider = new ethers.BrowserProvider(window.lukso);
-  
 
   try {
     // Request account access
-  const accounts = await provider.send('eth_requestAccounts', []);
-  // const accounts = await window.lukso.request({
-  //     method: "eth_requestAccounts",
-  //   });
+    const accounts = await provider.send("eth_requestAccounts", []);
+    // const accounts = await window.lukso.request({
+    //     method: "eth_requestAccounts",
+    //   });
 
     // Get the connected account
     return accounts[0];
@@ -74,29 +67,6 @@ const generateSignature = async (message: string) => {
     alert("Failed to sign message");
     throw error;
   }
-};
-
-const DirectPassportDataAccess = ({ address }: { address?: string }) => {
-  const { data, isError, error } = usePassportScore({
-    ...passportEmbedParams,
-    address: address,
-  });
-
-  return (
-    <div style={{ textAlign: "left" }}>
-      <h1>This is an app element!</h1>
-      <ul>
-        <li>Passport Score: {data?.score}</li>
-        <li>Passport Threshold: {data?.threshold}</li>
-        <li>Is passing threshold: {data?.passingScore ? "True" : "False"}</li>
-        <li>
-          What stamps?
-          <pre>{JSON.stringify(data?.stamps, undefined, 2)}</pre>
-        </li>
-        {isError && <li>Error: {(error as Error)?.message}</li>}
-      </ul>
-    </div>
-  );
 };
 
 function App() {
